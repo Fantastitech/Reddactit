a7f0ad78be7e9f28d56d1644482f44b0a219ebec
Fantastitech <chris@fantastitech.com>
2018-03-23 13:29:26 -0700

    General code cleanup

--- Reddactit/reddactit.js
+++ Reddactit/reddactit.js
@@ -1,14 +1,14 @@
-if (redacted == null) {
+if (redacted === null) {
     var redacted = false;
 }
 
-if (OP == null) {
+if (OP === null) {
     var OP = "";
 }
 
 function hashCode(str) {
     var hash = 0;
-    if (str.length == 0) return hash;
+    if (str.length === 0) return hash;
     for (i = 0; i < str.length; i++) {
         char = str.charCodeAt(i);
         hash = ((hash<<5)-hash)+char;
@@ -37,16 +37,16 @@ function redactUsernames() {
     var x = document.getElementsByClassName("author");
     var i;
     for (i = 0; i < x.length; i++) {
-        var userName = x[i].innerHTML.toLowerCase()
+        var userName = x[i].innerHTML.toLowerCase();
 		var color = "#" + intToRGB(hashCode(userName));
-        x[i].style.setProperty("color", "rgba(0, 0, 0, 0)", "important")
-        x[i].style.setProperty("background-color", color, "important")
+        x[i].style.setProperty("color", "rgba(0, 0, 0, 0)", "important");
+        x[i].style.setProperty("background-color", color, "important");
         if (x[i].classList.contains("submitter") || hasSomeParentTheClass(x[i], "top-matter")) {
-			OP = userName
-            x[i].style.setProperty("box-shadow", "inset 0 0 0 2px blue", "important")
-            x[i].style.setProperty("border-radius", "3px", "important")
-            x[i].style.setProperty("outline", "1px solid black", "important")
-            x[i].style.setProperty("outline-offset", "-3px", "important")
+			OP = userName;
+            x[i].style.setProperty("box-shadow", "inset 0 0 0 2px blue", "important");
+            x[i].style.setProperty("border-radius", "3px", "important");
+            x[i].style.setProperty("outline", "1px solid black", "important");
+            x[i].style.setProperty("outline-offset", "-3px", "important");
         }
 		x[i].setAttribute("redacted","");
     }
@@ -59,13 +59,13 @@ function redactMentions() {
 		if (/\/u\/[a-zA-Z0-9-_]{3,30}$/.test(x[i].href)) {
 			var userName = x[i].innerHTML.split("u/").pop().toLowerCase();
 			var color = "#" + intToRGB(hashCode(userName));
-			x[i].style.setProperty("color", "rgba(0, 0, 0, 0)", "important")
-			x[i].style.setProperty("background-color", color, "important")
+			x[i].style.setProperty("color", "rgba(0, 0, 0, 0)", "important");
+			x[i].style.setProperty("background-color", color, "important");
 			if (userName == OP) {
-				x[i].style.setProperty("box-shadow", "inset 0 0 0 2px blue", "important")
-				x[i].style.setProperty("border-radius", "3px", "important")
-				x[i].style.setProperty("outline", "1px solid black", "important")
-				x[i].style.setProperty("outline-offset", "-3px", "important")
+				x[i].style.setProperty("box-shadow", "inset 0 0 0 2px blue", "important");
+				x[i].style.setProperty("border-radius", "3px", "important");
+				x[i].style.setProperty("outline", "1px solid black", "important");
+				x[i].style.setProperty("outline-offset", "-3px", "important");
 			}
 			x[i].setAttribute("redacted","");
 		}
@@ -79,28 +79,28 @@ function restoreUsernames() {
     for (i = 0; i < x.length; i++) {
 		if (x[i].hasAttribute("redacted")) {
 			var userName = x[i].innerHTML.split("u/").pop().toLowerCase();
-			console.log(userName)
 			x[i].style.removeProperty("color");
 			x[i].style.removeProperty("background-color");
 			if (userName == OP) {
-				x[i].style.removeProperty("box-shadow")
-				x[i].style.removeProperty("border-radius")
-				x[i].style.removeProperty("outline")
-				x[i].style.removeProperty("outline-offset")
+				x[i].style.removeProperty("box-shadow");
+				x[i].style.removeProperty("border-radius");
+				x[i].style.removeProperty("outline");
+				x[i].style.removeProperty("outline-offset");
 			}
+			x[i].removeAttribute("redacted");
 		}
     }
 }
 
 function run() {
-    if (redacted == false) {
+    if (redacted === false) {
         redactUsernames();
 		redactMentions();
-		redacted = true
-    } else if (redacted == true) {
+		redacted = true;
+    } else if (redacted === true) {
         restoreUsernames();
-		redacted = false
+		redacted = false;
     }
 }
 
-run()
+run();
