49ab387554c1ea2602897995d38c0acb9de24e3a
Fantastitech <chris@fantastitech.com>
2018-03-23 09:43:48 -0700

    Added toggle functionality

--- Reddactit/reddactit.js
+++ Reddactit/reddactit.js
@@ -1,3 +1,7 @@
+if (redacted == null) {
+	var redacted = false;
+}
+
 function hashCode(str) {
     var hash = 0;
     for (var i = 0; i < str.length; i++) {
@@ -20,15 +24,46 @@ function hasSomeParentTheClass(element, classname) {
     return hasSomeParentTheClass(element.parentNode, classname);
 }
 
-var x = document.getElementsByClassName("author");
-var i;
-for (i = 0; i < x.length; i++) {
-	var userName = x[i].innerHTML
-	var color = "#" + intToRGB(hashCode(userName));
-	x[i].style.setProperty("color", color, "important")
-	x[i].style.setProperty("background-color", color, "important")
-	if (x[i].classList.contains("submitter") || hasSomeParentTheClass(x[i], "top-matter")) {
-		x[i].style.setProperty("outline", "3px solid blue", "important")
-		x[i].style.setProperty("outline-offset", "-3px", "important")
+function redactUsernames() {
+	var x = document.getElementsByClassName("author");
+	var i;
+	for (i = 0; i < x.length; i++) {
+		var userName = x[i].innerHTML
+		var color = "#" + intToRGB(hashCode(userName));
+		x[i].style.setProperty("color", "rgba(0, 0, 0, 0)", "important")
+		x[i].style.setProperty("background-color", color, "important")
+		if (x[i].classList.contains("submitter") || hasSomeParentTheClass(x[i], "top-matter")) {
+			x[i].style.setProperty("box-shadow", "inset 0 0 0 2px blue", "important")
+			x[i].style.setProperty("border-radius", "3px", "important")
+			x[i].style.setProperty("outline", "1px solid black", "important")
+			x[i].style.setProperty("outline-offset", "-3px", "important")
+		}
+	}
+	redacted = true
+}
+
+function restoreUsernames() {
+	var x = document.getElementsByClassName("author");
+	var i;
+	for (i = 0; i < x.length; i++) {
+		var userName = x[i].innerHTML
+		var color = "#" + intToRGB(hashCode(userName));
+		x[i].style.removeProperty("color");
+		x[i].style.removeProperty("background-color");
+		if (x[i].classList.contains("submitter") || hasSomeParentTheClass(x[i], "top-matter")) {
+			x[i].style.removeProperty("box-shadow")
+			x[i].style.removeProperty("border-radius")
+			x[i].style.removeProperty("outline")
+			x[i].style.removeProperty("outline-offset")
+		}
+	}
+	redacted = false;
+}
+
+function run() {
+	if (redacted == false) {
+		redactUsernames();
+	} else if (redacted == true) {
+		restoreUsernames();
 	}
 }
